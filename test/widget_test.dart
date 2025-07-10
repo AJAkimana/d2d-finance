4902f2afac50567362006ea32eaf11a4df9f78e0
Jean dAmour AKIMANA(MBP) <akimanaja17@gmail.com>
2025-07-10 12:02:57 +0200

    Fix commit message

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,16 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:d2d_finance/screens/login/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:d2d_finance/main.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp(client: null));
+    await tester.pumpWidget(const LoginScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
